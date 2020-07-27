097b90846b4702e9f2b3133c1232a5fbd318827c
Jean-Gabriel HOLMIERES <jean-gabriel.holmieres@cgi.com>
2020-07-27 13:58:34 +0200

    Total field et ministry

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 $( document ).ready(function() {
+    var fieldGuideTotal = 0;
+    var ministryManualTotal = 0;
 
     $('.BC-values').click( function () {
         var val = parseInt($(this).find('.value').text());
         var max = parseInt($(this).find('.max').text());
         if(val < max) {
             $(this).find('.value').text(val + 1 );
+            fieldGuideTotal = fieldGuideTotal + parseInt($(this).find('.field-guide').text());
+            ministryManualTotal = ministryManualTotal + parseInt($(this).find('.ministry-manual').text());
         }
     })
 
