b6ed44e1702714d0fdda272e6af3df12e9bfff22
Jean-Gabriel HOLMIERES <holmieres@gmail.com>
2020-07-28 21:50:37 +0200

    Ajout du double clic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 $( document ).ready(function() {
+    jQuery.fn.single_double_click = function(single_click_callback, double_click_callback, timeout) {
+        return this.each(function(){
+            var clicks = 0, self = this;
+            jQuery(this).click(function(event){
+                clicks++;
+                if (clicks == 1) {
+                    setTimeout(function(){
+                        if(clicks == 1) {
+                            single_click_callback.call(self, event);
+                        } else {
+                            double_click_callback.call(self, event);
+                        }
+                        clicks = 0;
+                    }, timeout || 300);
+                }
+            });
+        });
+    }
+
     var fieldGuideTotal = 0;
     var ministryManualTotal = 0;
 
-    $('.BC-values').click( function () {
+    $(".BC-values").single_double_click(function () {
         var val = parseInt($(this).find('.value').text());
         var max = parseInt($(this).find('.max').text());
         if(val < max) {
@@ -10,6 +29,14 @@ $( document ).ready(function() {
             fieldGuideTotal = fieldGuideTotal + parseInt($(this).find('.field-guide').text());
             ministryManualTotal = ministryManualTotal + parseInt($(this).find('.ministry-manual').text());
         }
+    }, function () {
+        var val = parseInt($(this).find('.value').text());
+        if(val > 0) {
+            $(this).find('.value').text(val - 1 );
+            fieldGuideTotal = fieldGuideTotal - parseInt($(this).find('.field-guide').text());
+            ministryManualTotal = ministryManualTotal - parseInt($(this).find('.ministry-manual').text());
+        }
     })
+
 
 });
